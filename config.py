7f0498a2be51652d46c442bd9dd421736a5a66da
greighuth <greighuth@hotmail.co.uk>
2020-11-26 12:14:48 +0000

    mmm

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 TOKEN = ""
 
 #category you want the breakout rooms to use must be integer 
-CATEGORY = 0
+CATEGORY = 1
 
 #id of the person who is allowed to use the commands must also be integer
 ME = 0
