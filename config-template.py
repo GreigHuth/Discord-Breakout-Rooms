bb9780dccd3ce767c24f270662bcba9ca15481ef
greighuth <greighuth@hotmail.co.uk>
2020-11-26 12:19:11 +0000

    added config template

--- config-template.py
+++ config-template.py
@@ -1,8 +1,8 @@
 #bot token
 TOKEN = ""
 
-#category you want the breakout rooms to use must be integer 
-CATEGORY = 1
+#category id you want the breakout rooms to use must be integer 
+CATEGORY = 0
 
 #id of the person who is allowed to use the commands must also be integer
 ME = 0
