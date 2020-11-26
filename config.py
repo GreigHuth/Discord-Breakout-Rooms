64ba5a1fafd2615cd64c693c88c72e065250fcfc
greighuth <greighuth@hotmail.co.uk>
2020-11-26 12:12:40 +0000

    mmm

--- config.py
+++
@@ -1,8 +0,0 @@
-#bot token
-TOKEN = ""
-
-#category you want the breakout rooms to use must be integer 
-CATEGORY = 360757822352392193
-
-#id of the person who is allowed to use the commands must also be integer
-ME = 105358952753041408
