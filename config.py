2f304cfe3cb718f42336396e9cc44c365e8fda09
Greig Huth <greighuth@hotmail.co.uk>
2020-11-26 12:14:29 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#bot token
+TOKEN = ""
+
+#category you want the breakout rooms to use must be integer 
+CATEGORY = 0
+
+#id of the person who is allowed to use the commands must also be integer
+ME = 0
